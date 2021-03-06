fe9d29725bae2ed354c7fe8f628a48edcb0e91c1
Cyrus Swihart <swihartc@oregonstate.edu>
2021-03-05 18:10:49 -0800

    Wrote 2nd test, {Tests whether multiples of 5 properly return 'buzz'}. Test fails.

--- fizztest.py
+++ fizztest.py
@@ -7,12 +7,20 @@ class TestCase(unittest.TestCase):
         self.assertEqual(fizzbuzz.fizzbuzz(6),"fizz")
         self.assertEqual(fizzbuzz.fizzbuzz(9),"fizz")
         self.assertEqual(fizzbuzz.fizzbuzz(12),"fizz")
-        self.assertEqual(fizzbuzz.fizzbuzz(15),"fizz")
         self.assertEqual(fizzbuzz.fizzbuzz(16),16)
         self.assertEqual(fizzbuzz.fizzbuzz(4),4)
         self.assertEqual(fizzbuzz.fizzbuzz(1),1)
         self.assertEqual(fizzbuzz.fizzbuzz(2),2)
-        
+    def testBuzz(self):
+        self.assertEqual(fizzbuzz.fizzbuzz(5),"buzz")
+        self.assertEqual(fizzbuzz.fizzbuzz(10),"buzz")
+        self.assertEqual(fizzbuzz.fizzbuzz(20),"buzz")
+        self.assertEqual(fizzbuzz.fizzbuzz(25),"buzz")
+        self.assertEqual(fizzbuzz.fizzbuzz(35),"buzz")
+        self.assertEqual(fizzbuzz.fizzbuzz(16),16)
+        self.assertEqual(fizzbuzz.fizzbuzz(4),4)
+        self.assertEqual(fizzbuzz.fizzbuzz(1),1)
+        self.assertEqual(fizzbuzz.fizzbuzz(2),2)
 
 if __name__ == '__main__':
     unittest.main()
